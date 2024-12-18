bf89c250b088ddd533412daa535d9d09ef2e4c3a
Alaaeldinabosamra <alaaeldinabosamra@gmail.com>
2024-12-18 13:30:48 +0200

    how node work with modules

--- index.js
+++ index.js
@@ -1,71 +1,30 @@
-// Server
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+console.log(arguments);
+console.log(require("module").wrapper);
 
-const replaceTemplate = (temp, product) => {
-  // دي علشان لو في اكتر من واحد في النص تسبدلهم كلهم مش واحد بس g
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
+// module.exports
+const C = require("./test-module");
+const calc1 = new C();
+console.log(calc1.add(2, 5));
 
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+// exports
+// const calc2 = require("./test-module2");
+const { add, multiply, divide } = require("./test-module2");
+console.log(add(2, 5));
 
-const tempOverview = fs.readFileSync(`${__dirname}/overview.html`, "utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/product.html`, "utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/card.html`, "utf-8");
-
-const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
-
-const server = http.createServer((req, res) => {
-  const { query, pathname } = url.parse(req.url, true);
-  // Overview Page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, {
-      "content-type": "text/html",
-    });
-
-    const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join("");
-    const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHtml);
-    res.end(output);
-
-    // Product Page
-  } else if (pathname === "/product") {
-    res.writeHead(200, {
-      "content-type": "text/html",
-    });
-    const product = dataObj[query.id];
-    const output = replaceTemplate(tempProduct, product);
-    res.end(output);
-
-    // API
-  } else if (pathname === "/api") {
-    res.writeHead(200, {
-      "content-type": "application/json",
-    });
-    res.end(data);
-    // Not Found
-  } else {
-    res.writeHead(404, {
-      "content-type": "text/html",
-    });
-    res.end("<h1> Page not Found!</h1>");
-  }
-});
-
-server.listen(8080, "127.0.0.1", () => {
-  console.log(`server is running !`);
-});
-
-//12
+// caching
+// this module only loaded once and code inside of it executed once only
+require("./test.module3")();
+require("./test.module3")();
+require("./test.module3")();
+/**
+ * How Node works folder with special wrapper function
+ * all the code inside a module is wrapped in common function
+ * wrapper function (exports, require, module, __filename, __dirname)
+ * Arguments is an array in Javascript
+ * and this array contain all the values there were passed into a function
+ * '0' => export
+ * '1' => require function
+ * '2' => module inside it exports
+ * '3' => path of root file name
+ * '4' => Directory name
+ */
