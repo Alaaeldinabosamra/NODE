7830069ed76bf31e23143509d5e809dfaf624ba4
Alaaeldinabosamra <alaaeldinabosamra@gmail.com>
2024-12-19 12:51:34 +0200

    start up

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Endpoints
+/**
+ app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hello from the server side!", app: "Natours" });
+});
+
+app.post("/", (req, res) => {
+  res.send("You can post to this endpoint....");
+});
+ */
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+// /api/v1/tours/:id/:x/:y?   :y? parameter value optional
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id == id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "Success",
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: "Fail",
+          message: "Fail to Update the file",
+        });
+      }
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
